905d406a6d454fb4e9e46873e4752fdc8148762b
YevgenPuzenko <blockshlako@gmail.com>
2020-11-14 14:18:12 +0200

    test2

---
+++ StartForm.cpp
@@ -0,0 +1,14 @@
+﻿#include "StartForm.h"
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+void main() {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+
+    Laba5OOPimgEncrypting::StartForm form; //WinFormsTest - èìÿ âàøåãî ïðîåêòà
+    Application::Run(% form);
+}
+
+
