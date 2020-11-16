8ccf1d197837ce600fb12b26238f4505d4d54e02
YevgenPuzenko <blockshlako@gmail.com>
2020-11-16 02:44:10 +0200

    Добавил возможность спокойно редактировать инфу в класе. Это привидение типов просто что-то... ужасное

--- StartForm.h
+++ StartForm.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "MyString.h"
+#include "MyEncrypt.h"
 
 namespace Laba5OOPimgEncrypting {
 
@@ -34,6 +36,9 @@ namespace Laba5OOPimgEncrypting {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
+	private: System::Windows::Forms::Label^ label1;
 
 	private:
 		/// <summary>
@@ -48,12 +53,49 @@ namespace Laba5OOPimgEncrypting {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"StartForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(165, 78);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &StartForm::button1_Click);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(380, 65);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(46, 17);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"label1";
+			// 
+			// StartForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1032, 627);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button1);
+			this->Name = L"StartForm";
+			this->Text = L"StartForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		MyEncrypt strg =  MyEncrypt();
+		strg.setText(label1->Text);
+		button1->Text = strg.getText();
+		//label1->Text = strg.getText();
+
+	}
 	};
 }
